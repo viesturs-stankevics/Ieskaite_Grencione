fc0a682d1b75a922bc3cd487fee848c4d5142e2f
Grencione <76771267+Grencione@users.noreply.github.com>
2021-03-09 09:15:04 +0200

    Update 10.b2_skolenu_darbi.py

--- 10.b2_skolenu_darbi.py
+++ 10.b2_skolenu_darbi.py
@@ -1,26 +1,26 @@
-1 	
-Kudrjavceva Veronika
-2 	
-Melnic Ana
-3 	
-Mihailovs Alekss
-4 	
-Orlova Katerina
-5 	
-Rimare Sabîne
-6 	
-Sidorenko Aleksandrs
-7 	
-Siïiòð Deniss
-8 	
-Skodorovs Vlads
-9 	
-Smirnovs Volodars
-10 	
-Verþanovskis Marks
-11 	
-Volkova Veronika
-12 	
-Zariòð Roberts
-13 	
-Þaravins Kirills
+1 	Kudrjavceva Veronika
+
+2 	Melnic Ana
+
+3 	Mihailovs Alekss
+
+4 	Orlova Katerina
+
+5 	Rimare SabÃ®ne
+
+6 	Sidorenko Aleksandrs
+
+7 	SiÃ¯iÃ²Ã° Deniss
+
+8 	Skodorovs Vlads
+
+9 	Smirnovs Volodars
+
+10 	VerÃ¾anovskis Marks
+
+11 	Volkova Veronika
+
+12 	ZariÃ²Ã° Roberts
+
+13 	Ãžaravins Kirills
+
